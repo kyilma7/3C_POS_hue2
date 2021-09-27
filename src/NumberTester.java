ac07022c39177ca9380b1d267336a7d6746f87f6
Kaan Yilmaz <YilmazK190043@sus.htl-grieskirchen.at>
2021-09-27 12:00:32 +0200

    3.

---
+++ src/NumberTester.java
@@ -0,0 +1,15 @@
+public class NumberTester {
+
+    public NumberTester(String fileName){
+
+    }
+
+    public void setOddEvenTester(NumberTest oddTester){
+        this.oddTester = oddTester;
+    }
+
+    public void setPrimeTester(NumberTest primeTester){
+        this.primeTester = primeTester;
+    }
+    
+}
