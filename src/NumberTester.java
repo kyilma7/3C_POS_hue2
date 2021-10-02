9b26baaf9f01a5b290a57b252f9cac2068b4a7a0
Kaan Yilmaz <YilmazK190043@sus.htl-grieskirchen.at>
2021-10-02 21:24:22 +0200

    8.

--- src/NumberTester.java
+++ src/NumberTester.java
@@ -1,16 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class NumberTester implements NumberTest {
 
+    private final String fileName;
     private NumberTest primeTester;
     private NumberTest oddTester;
     private NumberTest palindromeTester;
 
     public NumberTester(String fileName){
-
+        this.fileName = fileName;
     }
 
     public void setOddEvenTester(NumberTest oddTester){
         this.oddTester = oddTester;
-    } 
+    }
 
     public void setPrimeTester(NumberTest primeTester){
         this.primeTester = primeTester;
@@ -21,7 +29,15 @@ public class NumberTester implements NumberTest {
     }
 
     public void testFile(){
+        String line;
+        try (BufferedReader bfr = new BufferedReader(new FileReader("Tester.csv"))){
+            line = bfr.readLine();
 
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
